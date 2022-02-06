b60caad4e777b32c45e611b3ad2dd795d877416c
Marjan Amiri <marjanamiri778@gmail.com>
2022-02-05 21:53:25 -0800

    add assignment 1

---
+++ JavScript/Lab5/code.js
@@ -0,0 +1,14 @@
+function f(say){
+  
+  }
+function f(){
+    jQuery('#x').html("new content")
+  }
+  
+  function setup(){
+    jQuery('#p_1').click(f)
+
+    jQuery(#img_cat).click(say)
+  }
+  
+  jQuery(document).ready(setup)
