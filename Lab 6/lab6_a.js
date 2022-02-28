1bf1257616e7d6800856d91f0c1ea67a24624253
Marjan Amiri <marjanamiri778@gmail.com>
2022-02-27 18:29:32 -0800

    update lab 6

--- Lab 6/lab6_a.js
+++ Lab 6/lab6_a.js
@@ -1,131 +1,131 @@
 function add_operation() {
     console.log("setup () got called")
-        num1 = parseInt(jQuery("#num1").val());
-        num2 = parseInt(jQuery("#num2").val());
-        jQuery('#p1').html(
-            "<div class='box3'>" +
-            "Result: " + 
-            num1+
-            " + "+
-            num2+
-            " = "+
-            (num1+num2))
-            hello_ = 
-            num1+
-            " + "+
-            num2+
-            " = "+
-            (num1+num2);
-            "</div>"
-            old = jQuery('#history').html()
-            jQuery('#history').html(old +
-                "<div class='a'>" +
-                hello_ +
-                "</div>"
-            );
-    
-}
-function sub_operation(){
-    console.log("setup () got called")
     num1 = parseInt(jQuery("#num1").val());
     num2 = parseInt(jQuery("#num2").val());
     jQuery('#p1').html(
-        "<div class='box3'>" +
-        "Result: " + 
-        num1+
-        " - "+
-        num2+
-        " = "+
-        (num1-num2))
-        hello_ = 
-        num1+
-        " - "+
-        num2+
-        " = "+
-        (num1-num2);
+
+        "Result: " +
+        num1 +
+        " + " +
+        num2 +
+        " = " +
+        (num1 + num2))
+    hello_ =
+        num1 +
+        " + " +
+        num2 +
+        " = " +
+        (num1 + num2);
+
+    old = jQuery('#history').html()
+    jQuery('#history').html(old +
+        "<div class='a'>" +
+        hello_ +
         "</div>"
-        old = jQuery('#history').html()
-        jQuery('#history').html(old +
-            "<div class='y'>" +
-            hello_ +
-            "</div>"
-        );
-    
+    );
+
 }
-function multiply_operation(){
+
+function sub_operation() {
     console.log("setup () got called")
     num1 = parseInt(jQuery("#num1").val());
     num2 = parseInt(jQuery("#num2").val());
     jQuery('#p1').html(
-        "<div class='box3'>" +
-        "Result: " + 
-        num1+
-        " * "+
-        num2+
-        " = "+
-        (num1*num2))
-    hello_ = 
-    num1+
-    " * "+
-    num2+
-    " = "+
-    (num1*num2);
-    "</div>"
+        "Result: " +
+        num1 +
+        " - " +
+        num2 +
+        " = " +
+        (num1 - num2))
+    hello_ =
+        num1 +
+        " - " +
+        num2 +
+        " = " +
+        (num1 - num2);
+    old = jQuery('#history').html()
+    jQuery('#history').html(old +
+        "<div class='y'>" +
+        hello_ +
+        "</div>"
+    );
+
+}
+
+function multiply_operation() {
+    console.log("setup () got called")
+    num1 = parseInt(jQuery("#num1").val());
+    num2 = parseInt(jQuery("#num2").val());
+    jQuery('#p1').html(
+        "Result: " +
+        num1 +
+        " * " +
+        num2 +
+        " = " +
+        (num1 * num2))
+    hello_ =
+        num1 +
+        " * " +
+        num2 +
+        " = " +
+        (num1 * num2);
+
     old = jQuery('#history').html()
     jQuery('#history').html(old +
         "<div class='x'>" +
         hello_ +
         "</div>"
     );
-    
+
 }
-function dev_operation(){
+
+function dev_operation() {
     console.log("setup () got called")
     num1 = parseInt(jQuery("#num1").val());
     num2 = parseInt(jQuery("#num2").val());
     jQuery('#p1').html(
-        "<div class='box3'>" +
-        "Result: " + 
-        num1+
-        " * "+
-        num2+
-        " = "+
-        (num1*num2))
-    hello_ = 
-    num1+
-    " / "+
-    num2+
-    " = "+
-    (num1/num2);
-    "</div>"
+
+        "Result: " +
+        num1 +
+        " * " +
+        num2 +
+        " = " +
+        (num1 / num2))
+    hello_ =
+        num1 +
+        " / " +
+        num2 +
+        " = " +
+        (num1 / num2);
+
     old = jQuery('#history').html()
     jQuery('#history').html(old +
         "<div class='blue'>" +
         hello_ +
         "</div>"
     );
-    
+
 }
 
-function time_operation(){
+function time_operation() {
     console.log("setup () got called")
     num1 = parseInt(jQuery("#num1").val());
     num2 = parseInt(jQuery("#num2").val());
     jQuery('#p1').html(
-        "<div class='box3'>" +
-        "Result: " + 
-        num1+
-        " / "+
-        num2+
-        " = "+
-        (num1/num2))
-        "</div>"
-    hello_ = 
-    num1+
-    " ^ "+
-    num2+
-    " = "+
-    (num1**num2);
+
+        "Result: " +
+        num1 +
+        " ^ " +
+        num2 +
+        " = " +
+        (num1 ** num2))
+
+    hello_ =
+        num1 +
+        " ^ " +
+        num2 +
+        " = " +
+        (num1 ** num2);
 
     old = jQuery('#history').html()
     jQuery('#history').html(old +
@@ -135,25 +135,24 @@ function time_operation(){
     );
 
 }
-function per_operation(){
+
+function per_operation() {
     console.log("setup () got called")
     num1 = parseInt(jQuery("#num1").val());
     num2 = parseInt(jQuery("#num2").val());
     jQuery('#p1').html(
-        "<div class='box3'>" +
-        "Result: " + 
-        num1+
-        " % "+
-        num2+
-        " = "+
-        (num1/num2))
-        "</div>"
-    hello_ = 
-    num1+
-    " % "+
-    num2+
-    " = "+
-    (num1%num2);
+        "Result: " +
+        num1 +
+        " % " +
+        num2 +
+        " = " +
+        (num1 % num2))
+    hello_ =
+        num1 +
+        " % " +
+        num2 +
+        " = " +
+        (num1 % num2);
 
     old = jQuery('#history').html()
     jQuery('#history').html(old +
@@ -164,14 +163,28 @@ function per_operation(){
 
 }
 
+function dis() {
+    console.log("setup () got called")
+    let font_size = parseInt($("#history").css("font-size"));
+    $('#history').css("font-size", font_size - 1);
+}
+
+function increace() {
+    console.log("setup () got called")
+    let font_size = parseInt($("#history").css("font-size"));
+    $('#history').css("font-size", font_size + 1);
+}
+
 
 function setup() {
     console.log("setup () got called")
-    jQuery("#add").click(add_operation) 
-    jQuery("#sub").click(sub_operation) 
-    jQuery("#mul").click(multiply_operation) 
-    jQuery("#dev").click(dev_operation) 
+    jQuery("#add").click(add_operation)
+    jQuery("#sub").click(sub_operation)
+    jQuery("#mul").click(multiply_operation)
+    jQuery("#dev").click(dev_operation)
     $("#per").click(per_operation)
     $("#time").click(time_operation)
+    $("#in").click(increace)
+    $("#de").click(dis)
 }
 jQuery(document).ready(setup);
